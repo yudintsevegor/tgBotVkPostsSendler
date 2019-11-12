32d01759b2367a9b2f03af003325e3a0d0887fa6
Egor <yudintsevegor@gmail.com>
2019-11-12 22:54:50 +0300

    db func()-s

--- db.go
+++ db.go
@@ -3,11 +3,9 @@ package tgBotVkPostSendler
 import (
 	"database/sql"
 	"fmt"
-)
 
-type Writer struct {
-	DB *sql.DB
-}
+	"github.com/pkg/errors"
+)
 
 const (
 	CreateTable = `
@@ -21,23 +19,86 @@ const (
 	DeleteTable = `DROP TABLE %s`
 )
 
-func (w *Writer) EditTable(mode, tableName string) (sql.Result, error) {
-	query := fmt.Sprintf(mode, tableName)
+var mapReq = map[string]struct{}{
+	DeleteTable: struct{}{},
+	CreateTable: struct{}{},
+}
+
+type Writer struct {
+	DB        *sql.DB
+	TableName string
+
+	id       string
+	dateTime int
+	isPosted bool
+}
+
+func (w *Writer) EditTable(mode string) (sql.Result, error) {
+	if validateReq(mode) {
+		return nil, errors.New("[ERR] validation err")
+	}
+
+	query := fmt.Sprintf(mode, w.TableName)
 	return w.DB.Exec(query)
 }
 
-func (w *Writer) WriteToDB(tableName string) error {
-	query := fmt.Sprintf("INSERT INTO %s (ID, DateTime, IsPosted) VALUES ($1, $2, $3);", tableName)
+func validateReq(mode string) bool {
+	if _, ok := mapReq[mode]; !ok {
+		return false
+	}
+	return true
+}
+
+func (w *Writer) WriteToDB() error {
+	query := fmt.Sprintf("INSERT INTO %s (ID, DateTime, IsPosted) VALUES ($1, $2, $3);", w.TableName)
 
 	stmnt, err := w.DB.Prepare(query)
 	if err != nil {
 		return err
 	}
 
-	_, err = stmnt.Exec(1234, 5678, false)
+	_, err = stmnt.Exec(w.id, w.dateTime, w.isPosted)
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func (w *Writer) UpdateStatus() error {
+	query := fmt.Sprintf("UPDATE %s SET IsPosted = $1 WHERE ID = $2;", w.TableName)
+
+	stmnt, err := w.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+
+	_, err = stmnt.Exec(w.isPosted, w.id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (w *Writer) SelectRows() ([]string, error) {
+	query := fmt.Sprintf("SELECT ID FROM %s WHERE IsPosted = false;", w.TableName)
+
+	rows, err := w.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	ids := make([]string, 0, 1)
+
+	for rows.Next() {
+		var id string
+		if err := rows.Scan(&id); err != nil {
+			return nil, err
+		}
+		ids = append(ids, id)
+	}
+
+	return ids, nil
 }
